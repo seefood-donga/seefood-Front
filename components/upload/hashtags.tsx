e80679040648a2e318af9bb2d30535725f81cf16
HomieKim <ghals0708@gmail.com>
2022-06-29 21:21:38 +0900

    feat: 업로드페이지 state 추가

--- components/upload/hashtags.tsx
+++ components/upload/hashtags.tsx
@@ -1,30 +1,50 @@
 import useInput from "hooks/use-input";
-import React, { KeyboardEvent, useState } from "react";
+import React, { KeyboardEvent, MouseEvent, useRef, useState } from "react";
+import Xmark from 'public/icons/Xmark.svg';
 import styles from "styles/upload/hashtag.module.scss";
 
 const Hashtags = () => {
-  const [tagInput, onChangeTagInput] = useInput("");
-  const [tags, setTags] = useState([]);
-
-  const onKeyDown = (e:KeyboardEvent<HTMLInputElement>)=>{
-    if(e.key === 'Enter'){
-      console.log('enter Event')
+  const [tagInput, onChangeTagInput, settagInput] = useInput("");
+  const [tags, setTags] = useState<string[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onKeyUp = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      setTags([...tags, tagInput]);
+      settagInput("");
+      setTimeout(() => {
+        inputRef.current!.scrollIntoView({ behavior: "smooth" });
+      }, 100);
     }
-  }
+  };
+  const DeleteTag = (e: MouseEvent<HTMLSpanElement>)=>{
+    const tagetTag = e.currentTarget.innerText.substring(2);
+    setTags(tags => tags.filter(tag => tag !== tagetTag));
+  };
 
   return (
-      <ul className={styles["tag-wrapper"]}>
-        {tags.map((tag, i) => (
-          <li key={i}>{tag}</li>
-        ))}
+    <ul className={styles["tag-wrapper"]}>
+      {tags?.map((tag, i) => (
+        <li key={i} className={styles.tag}>
+          <span onClick={DeleteTag}>
+            # {tag}
+            <Xmark width={8}  fill="#44474B" />
+          </span>
+        </li>
+      ))}
+      <li>
+        <span># </span>
         <input
           className={styles.input}
           type="text"
           value={tagInput}
+          ref={inputRef}
           onChange={onChangeTagInput}
-          onKeyUp={onKeyDown}
+          onKeyUp={onKeyUp}
+          placeholder="태그 추가"
+          maxLength={5}
         />
-      </ul>
+      </li>
+    </ul>
   );
 };
 
--- components/upload/upload-category.tsx
+++ components/upload/upload-category.tsx
@@ -1,16 +1,22 @@
-import React, { useState } from 'react';
-import styles from 'styles/upload/upload-category.module.scss';
+import React, { useMemo, useState } from "react";
+import styles from "styles/upload/upload-category.module.scss";
 
 const UploadCategory = () => {
-  
-  const [selected, setSelected] = useState(false);
+  const category = useMemo(() => ["아침", "점심", "저녁"], []);
+  const [nowCategory, setNowCategory] = useState("");
   return (
     <ul className={styles.list}>
-      <li className={selected ? styles.active:''}>아침</li>
-      <li className={selected ? styles.active:''}>점심</li>
-      <li className={selected ? styles.active:''}>저녁</li>
+      {category.map((v, i) => (
+        <li
+          key={i}
+          className={nowCategory === v ? styles.active : ""}
+          onClick={() => setNowCategory(v)}
+        >
+          {v}
+        </li>
+      ))}
     </ul>
-  )
-}
+  );
+};
 
 export default UploadCategory;
