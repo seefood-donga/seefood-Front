68a20ccde37696c88746223c24bbe2269e2c5a29
HomieKim <ghals0708@gmail.com>
2022-09-22 10:23:59 +0900

    feat: SEO 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,41 @@ import "public/font/style.css";
 import "styles/globals.scss";
 import "styles/custom/calendar.css";
 import "react-circular-progressbar/dist/styles.css";
-import wrapper from 'store/store';
+import wrapper from "store/store";
+import { DefaultSeo } from "next-seo";
+
+const DEFAULT_SEO = {
+  title: "SeeFood",
+  description: "동아대학교 Software Project 2022 칼로리 분석 서비스 SeeFood",
+  openGraph: {
+    type: "websiete",
+    title: "SeeFood - Food Photo Analysis Service",
+    description: "donga univ SoftWare Project 2022 SeeFood",
+    site_name: "SeeFood",
+    images: [
+      {
+        url: "http://localhost:3050/favicon.ico",
+        width: 1200,
+        height: 630,
+        alt: "SeeFood Logo Image",
+      },
+    ],
+  },
+};
+
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const layoutHeader = Component.header;
   const hasBack = Component.back;
   const noNav = Component.noNav;
   const noPadding = Component.noPadding;
-  
 
   return (
-    <AppLayout {...{ layoutHeader, noNav, noPadding, hasBack }}>
-      <Component {...pageProps} />
-    </AppLayout>
+    <>
+      <DefaultSeo {...DEFAULT_SEO} />
+      <AppLayout {...{ layoutHeader, noNav, noPadding, hasBack }}>
+        <Component {...pageProps} />
+      </AppLayout>
+    </>
   );
 }
 
